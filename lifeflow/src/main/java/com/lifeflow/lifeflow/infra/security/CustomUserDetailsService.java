34bdcec3bd944fd8cab202e22a533912229faa85
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-02-03 16:41:02 -0300

    feat: implement authentication and authorization with Spring Security
    - Added `CustomUserDetailsService` for loading user details.
    - Configured `SecurityConfig` for stateless session management, password encoding, and endpoint permissions.
    - Implemented `SecurityFilter` for JWT token validation.
    - Created `TokenService` for generating and validating JWT tokens.

---
+++ lifeflow/src/main/java/com/lifeflow/lifeflow/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.lifeflow.lifeflow.infra.security;
+
+import com.lifeflow.lifeflow.core.user.domain.entity.User;
+import com.lifeflow.lifeflow.core.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
