ecf58187d607cb31a66dea48c1734e8d3d4b6fa4
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-02-03 16:00:26 -0300

    chore: Initialize LifeFlow project with Spring Boot, PostgreSQL configuration, and Docker setup.

---
+++ lifeflow/src/main/java/com/lifeflow/lifeflow/LifeFlowApplication.java
@@ -0,0 +1,13 @@
+package com.lifeflow.lifeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeFlowApplication.class, args);
+	}
+
+}
