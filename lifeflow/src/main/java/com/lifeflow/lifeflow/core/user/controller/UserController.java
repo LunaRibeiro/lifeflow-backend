82fa95e21cf0a1fbe0918214b2bb1574f9b45c52
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-02-03 17:19:20 -0300

    feat: add `UserController` to handle user login and registration endpoints

---
+++ lifeflow/src/main/java/com/lifeflow/lifeflow/core/user/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.lifeflow.lifeflow.core.user.controller;
+
+import com.lifeflow.lifeflow.core.user.domain.dto.request.UserLoginFormDTO;
+import com.lifeflow.lifeflow.core.user.domain.dto.request.UserRegisterFormDTO;
+import com.lifeflow.lifeflow.core.user.domain.dto.response.UserDTO;
+import com.lifeflow.lifeflow.core.user.domain.entity.User;
+import com.lifeflow.lifeflow.core.user.service.UserService;
+import com.lifeflow.lifeflow.infra.security.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final TokenService tokenService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> login (@RequestBody UserLoginFormDTO userLoginFormDTO){
+        User user = userService.findUserByEmailOrThrow(userLoginFormDTO.email());
+        if (passwordEncoder.matches(userLoginFormDTO.password(), user.getPassword())) {
+            String token = tokenService.generateToken(user);
+
+            UserDTO userDTO = userService.generateUserDTO(user, token);
+
+            return ResponseEntity.ok().body(userDTO);
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> register (@RequestBody UserRegisterFormDTO userRegisterFormDTO){
+        User user = userService.generateUser(userRegisterFormDTO);
+
+        String token = tokenService.generateToken(user);
+        UserDTO userDTO = userService.generateUserDTO(user, token);
+
+        return ResponseEntity.ok().body(userDTO);
+    }
+
+}
